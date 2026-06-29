076a6ac77c5b47e7b3c9de0b7b586a0c77244e2a
MooPlugins <manoj@bitkea.com>
2026-06-29 17:24:32 +0530

    Update version.php
    minimum required is 4.2

--- version.php
+++ version.php
@@ -32,5 +32,5 @@ defined('MOODLE_INTERNAL') || die();
 $plugin->component = 'mod_videolesson';
 $plugin->release = '1.0.1';
 $plugin->version = 2026061901;
-$plugin->requires = 2022112800;
+$plugin->requires = 2023042400;
 $plugin->maturity = MATURITY_STABLE;
